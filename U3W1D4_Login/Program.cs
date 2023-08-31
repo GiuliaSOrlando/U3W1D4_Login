17081c92b30359af2a83d610f111fd3faf83daa8
giuliaserenaorlando <giuliaserenaorlando@GIULIASEREN0108>
2023-08-31 14:34:55 +0200

    Aggiungere i file di progetto.

---
+++ U3W1D4_Login/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3W1D4_Login
+{
+    internal class Program
+    {
+
+        static void MostraMenu()
+        {
+            Console.WriteLine("===============OPERAZIONI==============");
+            Console.WriteLine("Scegli l’operazione da effettuare:");
+            Console.WriteLine("1.: Login");
+            Console.WriteLine("2.: Logout");
+            Console.WriteLine("3.: Verifica ora e data di login");
+            Console.WriteLine("4.: Lista degli accessi");
+            Console.WriteLine("5.: Esci");
+            Console.WriteLine("========================================");
+            Console.Write("Scelta: ");
+        }
+        static void Main(string[] args)
+        {
+            int scelta = 0;
+
+            do
+            {
+                try
+                {
+                    MostraMenu();
+                    scelta = int.Parse(Console.ReadLine());
+
+                    switch (scelta)
+                    {
+                        case 1:
+                            Console.Write("Inserisci username: ");
+                            string username = Console.ReadLine();
+                            Utente.Login(username);
+                            break;
+                        case 2:
+                            Utente.Logout();
+                            break;
+                        case 3:
+                            Utente.VerificaDataOra();
+                            break;
+                        case 4:
+                            Utente.ElencoAccessi();
+                            break;
+                        case 5:
+                            Console.WriteLine("Uscita dal programma.");
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Scelta non valida.");
+                            break;
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Si è verificato un errore: " + ex.Message);
+                }
+
+            }
+            while (scelta != 5);
+        }
+    }
+}
---
+++ U3W1D4_Login/Utente.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3W1D4_Login
+{
+    internal static class Utente
+    {
+        //Proprietà
+        public static string Username { get; set; }
+        public static Boolean IsLogged { get; set; }
+        public static DateTime LastLogin { get; set; }
+        public static List<DateTime> AccessLog = new List<DateTime>();
+
+        //Metodi
+
+        //Nascondi la digitazione della password
+        private static string NascondiPassword()
+        {
+            StringBuilder input = new StringBuilder();
+
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.Enter)
+                    break;
+
+                if (!char.IsControl(key.KeyChar))
+                {
+                    input.Append(key.KeyChar);
+                    Console.Write("*");
+                }
+            }
+
+            Console.WriteLine();
+            return input.ToString();
+        }
+
+        //Metodo per gestire il login
+        public static void Login(string username)
+        {
+            if (!string.IsNullOrWhiteSpace(username))
+            {
+                Console.Write("Inserisci password: ");
+                string password = NascondiPassword();
+                Console.Write("Conferma password: ");
+                string confirm_password = NascondiPassword();
+
+                if (password == confirm_password)
+                {
+                    Username = username;
+                    IsLogged = true;
+                    LastLogin = DateTime.Now;
+                    AccessLog.Add(LastLogin);
+                    Console.WriteLine("Login effettuato con successo");
+                }
+                else
+                {
+                    Console.WriteLine("Le password non coincidono");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Lo username non può essere vuoto");
+            }
+        }
+
+        // Metodo per gestire il logout
+        public static void Logout()
+        {
+            if(IsLogged)
+            {
+                Username = null;
+                IsLogged = false;
+                Console.WriteLine("Logout effetuato con successo");
+            }
+            else
+            {
+                Console.WriteLine("Nessun utente loggato");
+            }
+        }
+
+        //Metodo di verifica data e ora
+        public static void VerificaDataOra()
+        {
+            if (IsLogged)
+            {
+                Console.WriteLine($"Ultimo login effettuato il {LastLogin}");
+            }
+            else
+            {
+                Console.WriteLine("Nessun utente loggato");
+            }
+        }
+
+        public static void ElencoAccessi()
+        {
+            if(AccessLog.Count > 0)
+            {
+                Console.WriteLine($"Lista degli accessi: {string.Join(", ", AccessLog)}");
+            }
+
+            else
+            {
+                Console.WriteLine("Questo utente non ha ancora fatto accessi");
+            }
+        }
+    }
+}
